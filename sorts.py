76d40ccda691869bf9dc1c339d37fce5ef2528cb
Dantun1 <daniel@tunyan.com>
2025-08-23 12:40:56 +0100

    Pad ascending and reverse descending runs in find_and_prepare_runs() with 1 iteration (Timsort).

--- sorts.py
+++ sorts.py
@@ -108,7 +108,7 @@ def compute_min_run(n):
     #  255: 1111 1111, r= 1 127: 0111 1111, 63:0011 1111 -> return 64 if return 63, then 255/63 leaves small remainder 3 group. 255/64 leaves large groups so less merging,
     remainder = 0
     test = 0
-    while n > 64:
+    while n >= 64:
         remainder |= n & 1
         test += remainder
         print(test)
@@ -128,14 +128,14 @@ def insert_sort_subarray(sequence, left = 0, right = None):
             j -= 1
         sequence[j+1] = val
 
-def pad_ascending_runs(sequence, min_run):
-    for i in range(0,len(sequence), min_run):
-        j = i
-        while j< (upper := min(i + min_run - 1, len(sequence) - 1)):
-            if sequence[j+1] < sequence[j]:
-                insert_sort_subarray(sequence, i, upper)
-                break
-            j+= 1
+# def pad_ascending_runs(sequence, min_run):
+#     for i in range(0,len(sequence), min_run):
+#         j = i
+#         while j< (upper := min(i + min_run - 1, len(sequence) - 1)):
+#             if sequence[j+1] < sequence[j]:
+#                 insert_sort_subarray(sequence, i, upper)
+#                 break
+#             j+= 1
 
 
 def reverse_subsequence(sequence, left, right):
@@ -145,30 +145,62 @@ def reverse_subsequence(sequence, left, right):
         right -= 1
 
 
-def reverse_descending_runs(sequence):
-    i = 0
-    j = 0
-    while j < len(sequence)-1:
-        if sequence[j+1] > sequence[j] :
-            # print(i,sequence[i],j,sequence[j])
-            reverse_subsequence(sequence, i, j)
-            # print(sequence)
-            i = j+1
-        j+=1
-    reverse_subsequence(sequence, i, j)
+# def reverse_descending_runs(sequence):
+#     i = 0
+#     j = 0
+#     while j < len(sequence)-1:
+#         if sequence[j+1] > sequence[j] :
+#             # print(i,sequence[i],j,sequence[j])
+#             reverse_subsequence(sequence, i, j)
+#             # print(sequence)
+#             i = j+1
+#         j+=1
+#     reverse_subsequence(sequence, i, j)
+#
+#     return sequence
 
+def find_and_prepare_runs(sequence, min_run):
+    i = 0
+    while i < len(sequence):
+        run_start = i
+        run_end = i + 1
+        # The last element is trivially a non-descending run of length 1 so skip remaining logic in loop
+        if run_end >= len(sequence):
+            i+=1
+            continue
+        # Check for descending run
+        if sequence[run_end] < sequence[run_start]:
+            # Find the end of the descending run and reverse
+            while run_end < len(sequence) - 1 and sequence[run_end + 1] < sequence[run_end]:
+                run_end += 1
+            reverse_subsequence(sequence, run_start, run_end)
+        else:
+            while run_end < len(sequence) - 1 and sequence[run_end + 1] >= sequence[run_end]:
+                run_end += 1
+
+        # Pad runs shorter than min run
+        run_length = run_end - run_start + 1
+        if run_length < min_run:
+            new_run_end = min(run_start + min_run - 1, len(sequence) - 1)
+            insert_sort_subarray(sequence, run_start, new_run_end)
+            run_end = new_run_end
+        i = run_end + 1
     return sequence
 
 
-runs = [4,3,2,1,7,8,6,3,1]
-# runs = [4,3,2,1]
+
+
+
+
+
+runs = [random.randint(0,100) for _ in range(100)]
+print(find_and_prepare_runs(runs, 32))
 
 @timed_sort
 def tim_sort(sequence):
     MIN_RUN = compute_min_run(len(sequence))
 
-    reverse_descending_runs(sequence)
-    pad_ascending_runs(sequence, MIN_RUN)
+    find_and_prepare_runs(sequence, MIN_RUN)
 
     # for i in range(0, len(sequence), MIN_RUN):
     #
@@ -188,5 +220,3 @@ def tim_sort(sequence):
     #     size = 2*size
 
     return sequence
-
-print(tim_sort(runs))
